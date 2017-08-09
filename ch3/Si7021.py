8a6e6c9e89040ab6901db5931939e677a55fb543
Akkana Peck <akkana@shallowsky.com>
2017-08-09 10:08:44 -0600

    Narrower columns, to fit in the book

--- ch3/Si7021.py
+++ ch3/Si7021.py
@@ -15,14 +15,16 @@ class Si7021:
 
     def __init__(self, bus):
         # Open the I2C bus:
-        self.fread  = io.open("/dev/i2c-%d" % bus, "rb", buffering=0)
-        self.fwrite = io.open("/dev/i2c-%d" % bus, "wb", buffering=0)
+        self.fread  = io.open("/dev/i2c-%d" % bus, "rb",
+                              buffering=0)
+        self.fwrite = io.open("/dev/i2c-%d" % bus, "wb",
+                              buffering=0)
 
         # initialize the device as a slave:
         fcntl.ioctl(self.fread, self.I2C_SLAVE, self.ADDRESS)
         fcntl.ioctl(self.fwrite, self.I2C_SLAVE, self.ADDRESS)
 
-        self.fwrite.write(self.SOFT_RESET)    # soft reset
+        self.fwrite.write(self.SOFT_RESET)
         time.sleep(.1)
 
     def close(self):
@@ -46,16 +48,16 @@ class Si7021:
         if not buf:
             return -273.15    # absolute zero
 
-        return ((buf[0] << 8 | buf [1]) & 0xFFFC) * 175.72 / 65536.0 - 46.85
+        return (((buf[0] << 8 | buf [1]) & 0xFFFC)
+                * 175.72 / 65536.0 - 46.85)
 
     def read_humidity(self):
         buf = self.readI2C(self.READ_HUM_NOHOLD)
         if not buf:
             return -1
 
-        return ((buf[0] << 8 | buf [1]) & 0xFFFC) * 125.0 / 65536.0 - 6.0
-
-        # return ((hum0 * 256 + hum1) * 125 / 65536.0) - 6
+        return (((buf[0] << 8 | buf [1]) & 0xFFFC)
+                * 125.0 / 65536.0 - 6.0)
 
     def crc8check(self, value):
         remainder = ( ( value[0] << 8 ) + value[1] ) << 8
@@ -75,7 +77,9 @@ class Si7021:
 if __name__ == '__main__':
     sensor = Si7021(1)
     ctemp = sensor.read_temperature_c()
-    print("Temperature:  %.2f F (%.2f C)" % (ctemp * 1.8 + 32, ctemp))
-    print("Relative Humidity: %.1f %%" % sensor.read_humidity())
+    print("Temperature:  %.2f F (%.2f C)"
+          % (ctemp * 1.8 + 32, ctemp))
+    print("Relative Humidity: %.1f %%"
+          % sensor.read_humidity())
     sensor.close()
 
