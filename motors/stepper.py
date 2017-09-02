4d070cec4b942a0cddeeb5db3e18b7b2656cb77e
Akkana Peck <akkana@shallowsky.com>
2017-09-02 13:12:41 -0600

    Code cleanup, make a usable Stepper_28BYJ class

--- motors/stepper.py
+++ motors/stepper.py
@@ -1,74 +1,69 @@
 #!/usr/bin/env python
 
-# Control a 28BYJ stepper motor with a L293D or ULN2003A driver.
-# From https://tutorials-raspberrypi.com/how-to-control-a-stepper-motor-with-raspberry-pi-and-l293d-uln2003a/
-# Arduino version:
-# http://42bots.com/tutorials/28byj-48-stepper-motor-with-uln2003-driver-and-arduino-uno/
-#
-# There's also this: https://www.pololu.com/product/2753
-# but it can run one bipolar stepper, and the 28BYJ is unipolar.
+# Control a 28BYJ stepper motor with a ULN2003A driver.
+# Adapted from https://tutorials-raspberrypi.com/how-to-control-a-stepper-motor-with-raspberry-pi-and-l293d-uln2003a/
 
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-# coil_A_1_pin = 4 # pink
-# coil_A_2_pin = 17 # orange
-# coil_B_1_pin = 23 # blue
-# coil_B_2_pin = 24 # yellow
+class Stepper_28BYJ:
+    # Half-step mode for the 28BYJ has an 8-step control sequence.
+    halfstep = [
+        [0,1,0,0], [0,1,0,1], [0,0,0,1], [1,0,0,1],
+        [1,0,0,0], [1,0,1,0], [0,0,1,0], [0,1,1,0]
+        ]
 
-coil_A_1_pin = 19 # pink
-coil_A_2_pin =  6 # orange
-coil_B_1_pin = 13 # blue
-coil_B_2_pin =  5 # yellow
+    def __init__(self, a1pin, a2pin, b1pin, b2pin):
+        self.a1pin = a1pin    # pink
+        self.a2pin = a2pin    # orange
+        self.b1pin = b1pin    # blue
+        self.b2pin = b2pin    # yellow
 
-# adjust if different
-StepCount = 8
-Seq = range(0, StepCount)
-Seq[0] = [0,1,0,0]
-Seq[1] = [0,1,0,1]
-Seq[2] = [0,0,0,1]
-Seq[3] = [1,0,0,1]
-Seq[4] = [1,0,0,0]
-Seq[5] = [1,0,1,0]
-Seq[6] = [0,0,1,0]
-Seq[7] = [0,1,1,0]
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-# GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+        GPIO.setup(self.a1pin, GPIO.OUT)
+        GPIO.setup(self.a2pin, GPIO.OUT)
+        GPIO.setup(self.b1pin, GPIO.OUT)
+        GPIO.setup(self.b2pin, GPIO.OUT)
 
-# GPIO.output(enable_pin, 1)
+    def set_step(self, w1, w2, w3, w4):
+        GPIO.output(self.a1pin, w1)
+        GPIO.output(self.a2pin, w2)
+        GPIO.output(self.b1pin, w3)
+        GPIO.output(self.b2pin, w4)
 
-def setStep(w1, w2, w3, w4):
-    GPIO.output(coil_A_1_pin, w1)
-    GPIO.output(coil_A_2_pin, w2)
-    GPIO.output(coil_B_1_pin, w3)
-    GPIO.output(coil_B_2_pin, w4)
+    def forward(self, delay, steps):
+        for i in range(steps):
+            for step in self.halfstep:
+                self.set_step(step[0], step[1], step[2], step[3])
+                time.sleep(delay)
 
-def forward(delay, steps):
-    for i in range(steps):
-        for j in range(StepCount):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
+    def backward(self, delay, steps):
+        for i in range(steps):
+            for step in reversed(self.halfstep):
+                self.set_step(step[0], step[1], step[2], step[3])
+                time.sleep(delay)
 
-def backwards(delay, steps):
-    for i in range(steps):
-        for j in reversed(range(StepCount)):
-            setStep(Seq[j][0], Seq[j][1], Seq[j][2], Seq[j][3])
-            time.sleep(delay)
+    def cleanup(self):
+        GPIO.cleanup()
 
 if __name__ == '__main__':
-    while True:
-        # delay = raw_input("Time Delay (ms)?")
-        # steps = raw_input("How many steps forward? ")
-        delay = 5
-        steps = 100
-        forward(int(delay) / 1000.0, int(steps))
-        time.sleep(.1)
-        backwards(int(delay) / 1000.0, int(steps))
-        time.sleep(.1)
+    # Initialize the stepper with 4 GPIO pin numbers.
+    # 19, 6, 13, 5 is a sequence that allows plugging the controller
+    # board directly into the breadboard next to the Pi Cobbler.
+    stepper = Stepper_28BYJ(19, 6, 13, 5)
+
+    # Run the motor forward and backward, as a demo:
+    try:
+        while True:
+            delay = 5./1000.
+            steps = 100
+            stepper.forward(delay, steps)
+            time.sleep(.1)
+            stepper.backward(delay, steps)
+            time.sleep(.1)
+    except KeyboardInterrupt:
+        print("Cleaning up")
+        stepper.cleanup()
 
