3afb343d2794fec55a68c69b49ff505bd2d1ec8d
Akkana Peck <akkana@shallowsky.com>
2017-12-14 16:08:54 -0700

    Simple script to display a frame from the pi camera

---
+++ camera/picamera-view.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# start: https://www.raspberrypi.org/documentation/usage/camera/python/README.md
+# Full doc: http://picamera.readthedocs.io/en/release-1.13/
+# Useful recipes: http://picamera.readthedocs.io/en/release-1.10/recipes1.html
+
+# requires: python-picamera python-imaging-tk
+
+import io
+import time
+import picamera
+import Tkinter
+from PIL import Image, ImageTk
+
+root = Tkinter.Tk()
+
+# Create the in-memory stream to capture the image:
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    time.sleep(.5)
+    camera.capture(stream, format='jpeg')
+
+# "Rewind" the stream to the beginning so we can read its content
+stream.seek(0)
+image = Image.open(stream)
+
+# Now show it in the Tk window.
+photoimage = ImageTk.PhotoImage(image)
+label = Tkinter.Label(root, image=photoimage)
+label.pack()
+
+root.mainloop()
+
+camera.stop_preview()
