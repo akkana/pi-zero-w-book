f244df0f051733268cb0c2b0773d4e21243bc11b
Akkana Peck <akkana@shallowsky.com>
2017-12-21 20:02:31 -0700

    Car: use nonblocking input for controlling the car, and factor the car code into a class

--- motors/car-drv8835.py
+++ motors/car-drv8835.py
@@ -11,62 +11,97 @@
 import os
 import sys
 import time
+import termios
+import fcntl
+import tty
+import select
 from pololu_drv8835_rpi import motors, MAX_SPEED
 
-FAST_SPEED = 100
-SLOW_SPEED = 60
+class Car8835:
+    FAST_SPEED = 100
+    SLOW_SPEED = 60
 
-speed1 = 0
-speed2 = 0
+    def __init__(self):
+        self.speed1 = 0
+        self.speed2 = 0
 
-def accelerate_to(s1, s2):
-    global speed1, speed2
-    delay = .1
-    time.sleep(delay*2)
-    for i in range(10):
-        motors.setSpeeds((speed1 * (10-i) + s1 * i) / 10,
-                         (speed2 * (10-i) + s2 * i) / 10)
-        time.sleep(delay)
+    def accelerate_to(self, s1, s2):
+        global speed1, speed2
+        delay = .1
+        time.sleep(delay*2)
+        for i in range(10):
+            motors.setSpeeds((self.speed1 * (10-i) + s1 * i) / 10,
+                             (self.speed2 * (10-i) + s2 * i) / 10)
+            time.sleep(delay)
 
-    speed1 = s1
-    speed2 = s2
+        self.speed1 = s1
+        self.speed2 = s2
 
-def stop_now():
-    global speed1, speed2
-    motors.setSpeeds(0, 0)
-    speed1 = 0
-    speed2 = 0
+    def stop_now(self):
+        motors.setSpeeds(0, 0)
+        self.speed1 = 0
+        self.speed2 = 0
+
+class raw(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.fd = self.stream.fileno()
+    def __enter__(self):
+        self.original_stty = termios.tcgetattr(self.stream)
+        tty.setcbreak(self.stream)
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(self.stream, termios.TCSANOW, self.original_stty)
+
+class nonblocking(object):
+    def __init__(self, stream):
+        self.stream = stream
+        self.fd = self.stream.fileno()
+    def __enter__(self):
+        self.orig_fl = fcntl.fcntl(self.fd, fcntl.F_GETFL)
+        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
+    def __exit__(self, *args):
+        fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl)
 
 if __name__ == '__main__':
     if os.getuid() != 0:
         print("PWM needs root")
         sys.exit(1)
+
     try:
-        accelerate_to(0, 0)
-        while True:
-            cmd = raw_input("Command: ")
-            if cmd.startswith('f'):
-                accelerate_to(FAST_SPEED, FAST_SPEED)
+        car = Car8835()
+        car.accelerate_to(0, 0)
 
-            elif cmd.startswith('b'):
-                accelerate_to(-SLOW_SPEED, -SLOW_SPEED)
+        with raw(sys.stdin):
+            with nonblocking(sys.stdin):
 
-            elif cmd.startswith('l'):
-                accelerate_to(FAST_SPEED, SLOW_SPEED)
+                while True:
+                    r, w, e = select.select([sys.stdin], [], [])
+                    if r:
+                        cmd = sys.stdin.read()
 
-            elif cmd.startswith('r'):
-                accelerate_to(SLOW_SPEED, FAST_SPEED)
+                        if cmd.startswith('f'):
+                            car.accelerate_to(Car8835.FAST_SPEED, Car8835.FAST_SPEED)
 
-            elif cmd.startswith('s'):
-                stop_now()
+                        elif cmd.startswith('b'):
+                            car.accelerate_to(-Car8835.SLOW_SPEED, -Car8835.SLOW_SPEED)
 
-            elif cmd.startswith('q'):
-                break
+                        elif cmd.startswith('l'):
+                            car.accelerate_to(Car8835.FAST_SPEED, Car8835.SLOW_SPEED)
 
-            else:
-                accelerate_to(0, 0)
-            time.sleep(1)
+                        elif cmd.startswith('r'):
+                            car.accelerate_to(Car8835.SLOW_SPEED, Car8835.FAST_SPEED)
+
+                        elif cmd.startswith('s'):
+                            car.stop_now()
+
+                        elif cmd.startswith('q'):
+                            break
+
+                        else:
+                            car.accelerate_to(0, 0)
+
+                        time.sleep(1)
 
     finally:
         # In case of problems or user hit ^C, stop both motors.
-        accelerate_to(0, 0)
+        car.accelerate_to(0, 0)
