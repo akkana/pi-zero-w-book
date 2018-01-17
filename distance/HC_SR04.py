8039cd15a52b0ae3b06e08e2ed9a74a0b3f386c0
Akkana Peck <akkana@shallowsky.com>
2018-01-17 14:36:13 -0700

    Motion detectors: same old HC_SR04 and PIR code from scripts/rpi, plus a new script for the ME007.

---
+++ distance/HC_SR04.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+# Raspberry Pi driver for the HC-SR04 ultrasonic rangefinder
+# and similar devices.
+# Copyright (C) 2014 Akkana Peck <akkana@shallowsky.com>>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+# Adapted from code by Matt Hawkins of RaspberryPi-Spy.co.uk
+# Wire the circuit as shown in
+# http://www.raspberrypi-spy.co.uk/2013/01/ultrasonic-distance-measurement-using-python-part-2/
+
+# This circuit also works with an ME007Y (the one with the long cable)
+# but not with an ME007-ULS (the one in the big plastic housing with
+# a button to switch modes).
+
+import RPi.GPIO as GPIO
+import time
+
+class HC_SR04:
+    def __init__(self, trigger=23, echo=24):
+        self.GPIO_TRIGGER = trigger
+        self.GPIO_ECHO    = echo
+
+        # Use BCM instead of physical pin numbering:
+        GPIO.setmode(GPIO.BCM)
+
+        # Set trigger and echo pins as output and input
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+
+        # Initialize trigger to low:
+        GPIO.output(self.GPIO_TRIGGER, False)
+
+    def measure_distance_cm(self, verbose=False):
+        '''Measure a single distance, in cemtimeters.
+        '''
+        return self.measure_distance_in(verbose) * 2.54
+
+    def measure_distance_in(self, verbose=False):
+        '''Measure a single distance, in inches.
+        '''
+        if verbose:
+            print "Sending trigger pulse ..."
+        GPIO.output(self.GPIO_TRIGGER, True)
+        time.sleep(0.00001)
+        GPIO.output(self.GPIO_TRIGGER, False)
+        if verbose:
+            print "Waiting for ECHO to go low:"
+
+        start = time.time()
+        while GPIO.input(self.GPIO_ECHO) == 0:
+            start = time.time()
+
+        if verbose:
+            print "Waiting for ECHO to go high:"
+        stop = time.time()
+        while GPIO.input(self.GPIO_ECHO) == 1:
+            stop = time.time()
+        if verbose:
+            print "Got the echo."
+
+        # Convert to inches:
+        return (((stop - start) * 34300)/2)*0.393701
+
+    def average_distance_in(self, samples=3, verbose=False):
+        tot = 0.0
+        for i in xrange(samples):
+            tot += self.measure_distance_in(verbose)
+            time.sleep(0.1)
+        return tot / samples
+
+if __name__ == '__main__':
+    try:
+        print "Initializing the rangefinder ..."
+        rf = HC_SR04()
+        while True:
+            print "Distance: %.1f inches" % rf.average_distance_in(verbose=True)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # User pressed CTRL-C: reset GPIO settings.
+        print "Cleaning up ..."
+        GPIO.cleanup()
---
+++ distance/ME007.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+# Raspberry Pi driver for the HC-SR04 ultrasonic rangefinder
+# and similar devices.
+# Copyright (C) 2014 Akkana Peck <akkana@shallowsky.com>>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+# Adapted from code by Matt Hawkins of RaspberryPi-Spy.co.uk
+# Wire the circuit as shown in
+# http://www.raspberrypi-spy.co.uk/2013/01/ultrasonic-distance-measurement-using-python-part-2/
+
+# This circuit also works with an ME007Y (the one with the long cable)
+# but not with an ME007-ULS (the one in the big plastic housing with
+# a button to switch modes).
+
+import RPi.GPIO as GPIO
+import time
+
+class HC_SR04:
+    def __init__(self, trigger=23, echo=24):
+        self.GPIO_TRIGGER = trigger
+        self.GPIO_ECHO    = echo
+
+        # Use BCM instead of physical pin numbering:
+        GPIO.setmode(GPIO.BCM)
+
+        # Set trigger and echo pins as output and input
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
+
+        # Initialize trigger to high:
+        GPIO.output(self.GPIO_TRIGGER, True)
+
+    def measure_distance_cm(self, verbose=False):
+        '''Measure a single distance, in cemtimeters.
+        '''
+        return self.measure_distance_in(verbose) * 2.54
+
+    def measure_distance_in(self, verbose=False):
+        '''Measure a single distance, in inches.
+        '''
+        # Make sure the trigger starts high, then goes low
+        # to start a measurement:
+        GPIO.output(self.GPIO_TRIGGER, True)
+        time.sleep(.01)
+        GPIO.output(self.GPIO_TRIGGER, False)
+
+        # Wait for the echo to go high, then low.
+        # Time how long it stays high.
+        GPIO.wait_for_edge(self.GPIO_ECHO, GPIO.RISING, timeout=1000)
+        start = time.time()
+        GPIO.wait_for_edge(self.GPIO_ECHO, GPIO.FALLING, timeout=1000)
+        stop = time.time()
+
+        # Set the trigger back high since we're no longer measuring.
+        GPIO.output(self.GPIO_TRIGGER, True)
+
+        # Convert to inches:
+        return (((stop - start) * 34300)/2)*0.393701
+
+    def average_distance_in(self, samples=3, verbose=False):
+        tot = 0.0
+        for i in xrange(samples):
+            tot += self.measure_distance_in(verbose)
+            time.sleep(0.1)
+        return tot / samples
+
+if __name__ == '__main__':
+    try:
+        rf = HC_SR04()
+        while True:
+            print "Distance: %.1f inches" % rf.average_distance_in(verbose=True)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # User pressed CTRL-C: reset GPIO settings.
+        print "Cleaning up ..."
+        GPIO.cleanup()
+
