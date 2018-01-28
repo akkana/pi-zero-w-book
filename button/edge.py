e6df67174a8689957f1d8abd3a3e225635abc003
Akkana Peck <akkana@shallowsky.com>
2018-01-27 18:44:16 -0700

    Comments and formatting

--- button/edge.py
+++ button/edge.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import RPi.GPIO as GPIO
 import time
 
 def button_handler(pin):
     # Since we specified bouncetime, we have to wait a little
-    # before checking the pin's value.
+    # before checking the pin's value; RPi.GPIO doesn't wait
+    # for the pin to settle before calling the event handler.
     # But the amount we need to wait isn't the same as the bounce time.
     # If we wait the full bouncetime, we'll get multiple events.
     time.sleep(.01)
@@ -20,12 +21,11 @@ if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
 
     # Wire the button to +3.3, then enable an internal pulldown.
-    GPIO.setup(button_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+    GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     # events can be GPIO.RISING, GPIO.FALLING, or GPIO.BOTH
-    GPIO.add_event_detect(button_pin, GPIO.BOTH,
-                          callback=button_handler,
-                           bouncetime=300)
+    GPIO.add_event_detect(button_pin, GPIO.BOTH, callback=button_handler,
+                          bouncetime=300)
 
     try:
         time.sleep(1000)
